d8d26b549cd9a479f6b07b9807ab843ebf9c24e6
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2021-03-07 19:00:47 +0100

    .

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
+set -eou pipefail
 cd examples
 make
-valgrind ./examples/main
+valgrind ./main
 cd ..
 cargo run -- -i examples/lib.o -i examples/main.o -o foo
 valgrind ./foo
